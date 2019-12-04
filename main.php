a86b36a3649128a1fd32e8c0b9e81feaa0d7677c
Alex <canton.alex@live.fr>
2019-12-04 18:30:57 +0100

    test commit with VS

--- main.php
+++ main.php
@@ -8,7 +8,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <style>
-
     body { 
       margin:0;
       padding:0;
