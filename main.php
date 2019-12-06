e3ea8435387f337a01d7480999f4e86a7759f733
Alex <canton.alex@live.fr>
2019-12-06 18:38:46 +0100

    style.css, barre de recherche fini, open street map s'affiche bizarrement

--- main.php
+++ main.php
@@ -5,82 +5,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <style>
-    html, body {
-	    height: 100%;
-	    margin: 0; padding: 0;
-    }
-    body { 
-      background: url(img/crowd.jpg) no-repeat center fixed; 
-      -webkit-background-size: cover; 
-      background-size: cover;
-	    display : table;
-	    width: 100%;
 
-      /* background: #ffd89b;
-      background: -webkit-linear-gradient(to right, #ffd89b, #19547b);
-      background: linear-gradient(to right, #ffd89b, #19547b); */
-    }
-    .navbar {
-      margin-bottom: 0;
-      background-color: rgba(0, 0, 0, 1); 
-    }
-    .nav {
-      background-color: rgba(150, 0, 75, 0.5);  
-    }
-    .nav a {
-      color: white;
-    }
-    ul.nav a:hover {
-      /* background-color: rgba(150, 0, 75, 1);   */
-      background-color: DimGrey !important;  
-      /* color: Plum !important; */
-      color: Gold !important;
-    }
-    .logo {
-      width: 60%;
-    }
-    .bloc {
-      background-color: rgba(0, 0, 0, 0.5);
-      color: rgb(255, 255, 255);
-      text-align: center;
-      width: 800px;
-      margin: 0 auto;
-      border-radius: 5px;
-      margin-top: 5px;
-    }
-    .box {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    footer {
-      background-color: rgba(0, 0, 0, 0.8);
-      padding: 25px;
-      color: white;
-	    display : table-row;
-	    height: 1px;
-    }
-
-    @import "compass/css3";
-
-    .flex-container {
-      padding: 0;
-      margin: 0;
-      list-style: none;
-  
-      display: -webkit-box;
-      display: -moz-box;
-      display: -ms-flexbox;
-      display: -webkit-flex;
-      display: flex;
-  
-      -webkit-flex-flow: row wrap;
-      justify-content: space-around;
-    }
-  </style>
+  <meta name="viewport" content="width=device-width">
+  <!-- OpenLayers CSS -->
+  <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v6.0.1/css/ol.css" type="text/css">
 </head>
 <body>
 
@@ -110,73 +41,51 @@
 <!-- Fin du menu -->
 
 <!-- Début de la barre de recherche -->
-<div class="bloc" id="search-bar">
-  [Barre de recherche]
-  <ul class="flex-container">
-    <form action="#" method="post" novalidate="novalidate">
-      <li class="flex-item">
-        <select class="form-control search-slt">
-          <option>(Sélectionne une catégorie)</option>
-          <option>Spectacle</option>
-          <option>Concert</option>
-          <option>Exposition</option>
-          <option>Festival</option>
-          <option>Evènement sportif</option>
-        </select>
-      </li>
-      <li class="flex-container">
-        <div class="form-group row">
-          <div class="col-10">
-            <input class="form-control" type="date">
-          </div>
-        </div>
-      </li>
-    </form>    
-  </ul>    
-  
-  <!-- <section class="search-sec">
-    <div class="container">
-      <form action="#" method="post" novalidate="novalidate">
-        <div class="row">
-          <div class="col-lg-8">
-            <div class="row">
-              <div class="col-lg-3 col-md-3 col-sm-8 p-0">
-                <input type="text" class="form-control search-slt" placeholder="Enter Pickup City">
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-8 p-0">
-                <input type="text" class="form-control search-slt" placeholder="Enter Drop City">
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-8 p-0">
-                <select class="form-control search-slt" id="exampleFormControlSelect1">
-                  <option>Select Vehicle</option>
-                  <option>Example one</option>
-                  <option>Example one</option>
-                  <option>Example one</option>
-                  <option>Example one</option>
-                  <option>Example one</option>
-                  <option>Example one</option>
-                </select>
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-8 p-0">
-                <button type="button" class="btn btn-danger wrn-btn">Search</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </form>
+<div class="bloc" id="bloc-search-bar">
+  <div class="wrapper">
+
+    <div class="search-bar-category">
+      <select class="form-control search-slt">
+        <option>(Sélectionne une catégorie)</option>
+        <option>Spectacle</option>
+        <option>Concert</option>
+        <option>Exposition</option>
+        <option>Festival</option>
+        <option>Evènement sportif</option>
+      </select>
     </div>
-  </section> -->
+
+    <div class="search-bar-date">
+      <input class="form-control" type="date">
+    </div>
+
+    <div class="search-bar-submit">
+      <input type="submit" value="Rechercher">
+    </div>
+
+  </div>
+
 </div>
 <!-- Fin de la barre de recherche -->
 
 <!-- Début de la map -->
-<div class="bloc" id="map">
+<div class="bloc" id="bloc-map">
   [Open map]
+  <div id="map"></div>
+    <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v6.0.1/build/ol.js" type="text/javascript"></script>
+
+    <script>
+      var map = new ol.Map({
+        target: 'map',
+        layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
+        view: new ol.View({center: ol.proj.fromLonLat([2.1833, 41.3833]), zoom: 6})
+      });
+    </script>
 </div>
 <!-- Fin de la map -->
 
 <!-- Début de la liste des événements -->
-<div class="bloc" id="listEvents">
+<div class="bloc" id="bloc-list-events">
   [Liste des événements]
 </div>
 <!-- Fin de la liste des événements -->
