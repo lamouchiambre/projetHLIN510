864386f60eb30a22c9c283da3011a95ada7f2eaa
Alex <canton.alex@live.fr>
2019-12-11 16:43:31 +0100

    la map fonctionne

--- main.php
+++ main.php
@@ -8,6 +8,13 @@
   <link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+    crossorigin=""/>
+  <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+    crossorigin=""></script>
 
   <meta name="viewport" content="width=device-width">
   <!-- OpenLayers CSS -->
@@ -82,16 +89,17 @@
 
 <!-- Début de la map -->
 <div class="bloc" id="bloc-map">
-  [Open map]
-  <div id="map"></div>
-    <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v6.0.1/build/ol.js" type="text/javascript"></script>
-
+  <div id="mapid"></div>
     <script>
-      var map = new ol.Map({
-        target: 'map',
-        layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
-        view: new ol.View({center: ol.proj.fromLonLat([2.1833, 41.3833]), zoom: 6})
-      });
+      window.onload = function() {
+        var mymap = L.map('mapid').setView([43.6, 3.8833], 13);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+
+        var marker = L.marker([43.6, 3.8833]).addTo(mymap);
+      }  
     </script>
 </div>
 <!-- Fin de la map -->
