fc6894f5a20805a8d71b5d25db25c81a423ded81
Ambre Lamouchi <lamouchiambre@gmail.com>
2019-12-14 20:41:43 +0100

    push add_lieu.php

---
+++ add_lieu.php
@@ -0,0 +1,89 @@
+<?php 
+session_start();
+echo $_SESSION['us_id'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Site événementiel d'Alexambre</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  <meta name="viewport" content="width=device-width">
+  <!-- OpenLayers CSS -->
+  <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v6.0.1/css/ol.css" type="text/css">
+</head>
+<body>
+
+<!-- Début du menu -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <img class="logo" src="img/logoMtp.png">
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="main.php">Acceuil</a></li>
+        <li><a href="#">A propos</a></li>
+        <li><a href="#">Nous contacter</a></li>
+        <?php 
+          if(!empty($_SESSION['us_id'])){
+            echo '<li><a href="espace_menbre.php">Mon espace</a></li>';
+          }
+        ?>
+      </ul>
+      <?php 
+
+        if(empty($_SESSION['us_id'])){
+          echo '<ul class="nav navbar-nav navbar-right"> <li><a href="connexion.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> </ul>';
+        }else{
+          echo '<ul class="nav navbar-nav navbar-right"> <li><a href="deconnection.php"><span class="glyphicon glyphicon-log-in"></span> Deconnection</a></li> </ul>';
+        }
+        
+      ?>
+
+    </div>
+  </div>
+</nav>
+
+<!-- Fin du menu -->
+
+<div class = 'bloc'>
+<div class="bloc" id="bloc-map">
+  <div id="mapid"></div>
+    <script>
+      window.onload = function() {
+        var mymap = L.map('mapid').setView([43.6, 3.8833], 13);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+
+        var marker = L.marker([43.6, 3.8833]).addTo(mymap);
+
+      }
+    </script>
+
+</div>
+
+    
+
+</div>
+
+<!-- Début du footer -->
+<footer class="container-fluid text-center" id="footer">
+  <p>&copy; 2019 Copyright: A. Canton Condes, A. Lamouchi<p>
+</footer>
+<!-- Fin du Footer -->
+
+</body>
+</html>
